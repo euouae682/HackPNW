2b130d27bc9e170f0f49402c88652e6baf319d4a
euouae682 <jasonuzhao682@gmail.com>
2023-06-04 10:34:31 -0700

    map layout

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,7 +4,16 @@ import { store } from '../store.js'
 
 <template>
   <main class="map" :id=store.showNav>
-    <h1>Placeholder</h1>
+    <h1 class="map-title">EcoTribe Map</h1>
+    <button class="add-point">
+      <p>add a point</p>
+    </button>
+    <aside class="legend">
+      <p>test</p>
+    </aside>
+    <div class="world-map">
+      <p>test</p>
+    </div>
   </main>
 </template>
 
@@ -17,5 +26,36 @@ import { store } from '../store.js'
 #false {
   -webkit-filter: none;
   filter: none;
+}
+
+.map {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 4.8rem;
+  margin: 4.8rem;
+  height: 100vh;
+}
+
+.map-title {
+  color: #32773f;
+  font-size: 7.4rem;
+  font-weight: 700;
+  width: 70%;
+}
+
+.add-point {
+  width: 10%;
+  background-color: green;
+}
+
+.legend {
+  width: 20%;
+  height: 80%;
+  background-color: green;
+}
+
+.world-map {
+  width: 70%;
 }
 </style>
