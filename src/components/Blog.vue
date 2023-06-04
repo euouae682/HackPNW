1f83490adfe57c6d438398fc128cc0b65eee08c7
euouae682 <jasonuzhao682@gmail.com>
2023-06-04 10:42:38 -0700

    implemented comically basic search algo

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -2,21 +2,17 @@
 import BlogHeader from "./blog/BlogHeader.vue";
 import BlogPost from './blog/BlogPost.vue';
 import { ref } from 'vue';
+import { store } from '../store.js';
 
 const posts = ref([{ 
-  title: 'Intertwined with the Land', 
-  article: 'Native American communities have long-standing relationships with their ancestral lands, viewing them not merely as resources but as living entities. Climate change disrupts this intricate balance, altering ecosystems, diminishing biodiversity, and threatening cultural practices tied to the land...', 
-  topic: 'Native living', 
-  date: 'May 14th', 
-  time: '15min', 
-  imgUrl: '' 
+  
 }])
 
 </script>
 
 <template>
   <BlogHeader />
-  <section class="blog-container" v-for="post in posts">
+  <section class="blog-container" v-for="post in store.posts">
       <BlogPost :title="post.title" :article="post.article" :topic="post.title" :date="post.date" :time="post.time" :imgUrl="post.imgUrl"/>
     </section>
 </template>
--- src/store.js
+++ src/store.js
@@ -22,5 +22,21 @@ export const store = reactive({
         this.tradExpand = false;
         this.womenExpand = false;
         this.warmingExpand = true;
+    },
+    posts: [
+        {title: 'Intertwined with the Land', 
+        article: 'Native American communities have long-standing relationships with their ancestral lands, viewing them not merely as resources but as living entities. Climate change disrupts this intricate balance, altering ecosystems, diminishing biodiversity, and threatening cultural practices tied to the land...', 
+        topic: 'Native living', 
+        date: 'May 14th', 
+        time: '15min', 
+        imgUrl: ''}],
+    filteredPosts: [],
+    filterPosts(searchTopic) {
+        filteredPosts = []
+        this.posts.forEach((post) => {
+            if (post.topic == searchTopic) {
+                this.filteredPosts.concat(post);
+            }
+        })
     }
 })
