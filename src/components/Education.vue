7f2c53d099b5cb27ce30871ff9bce121972d6c28
RedPandaGaminq <timothyzhong2@gmail.com>
2023-06-04 11:26:10 -0700

    HOW DSJFKAPJFIA

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -2,7 +2,8 @@
 import { store } from "../store.js";
 import { onMounted, onUnmounted } from "vue";
 import EducationHero from "./education/EducationHero.vue";
-import EduElement from "./education/EduElement.vue";
+import EducationThumbnail from "./education/EducationThumbnail.vue";
+import EducationMain from "./education/EducationMain.vue";
 
 var fadeInElements = [];
 
@@ -36,53 +37,16 @@ const isElemVisible = (el) => {
 </script>
 
 <template>
+  <EducationHero />
+  <EducationThumbnail />
   <main class="education" :id="store.showNav">
-    <EducationHero />
-    <EduElement
-      id="traditions"
-      title="Native American Traditions"
-      desc="Native American traditions encompass a rich tapestry of cultural practices that have been passed down through generations. Rooted in deep reverence for the land, these traditions emphasize a harmonious relationship between humans and nature. 
-      Native American communities have an intricate understanding of their surroundings, recognizing the interconnectedness of all living beings. Traditional ceremonies, such as powwows, sweat lodges, and vision quests, play a vital role in spiritual expression and community bonding. The oral tradition, storytelling, remains a fundamental aspect, preserving ancestral knowledge, history, and cultural values. Native American arts and crafts, such as intricate beadwork, pottery, and intricate carvings, not only showcase remarkable craftsmanship but also reflect profound connections to nature and ancestral heritage.
-      Respect for elders, communal decision-making, and a profound sense of interconnectedness shape the social fabric of these communities. Native American traditions are a testament to the enduring spirit and resilience of Indigenous cultures, serving as a wellspring of wisdom and inspiration for all who seek to understand and honor their heritage.
-"
-      bg="#32773f"
-      hcol="#0f2413"
-      tcol="#fff"
-      align="right"
-      imgUrl="education/traditions.png"
-    />
-
-    <EduElement
-      id="women"
-      title="Native American Women"
-      desc="Native American women and elders hold significant roles within their communities, serving as pillars of strength, wisdom, and cultural preservation. Native American women have been essential in upholding and passing down traditions, maintaining the social fabric, and nurturing the next generation. They are revered for their resilience, leadership, and deep connection to the land.
-
-Native American elders, both men and women, are revered for their wisdom and life experiences. They are the custodians of ancestral knowledge, embodying the traditions, stories, and teachings that have been passed down through generations. Elders play a crucial role in decision-making, guiding community members, and providing guidance on spiritual and cultural matters. 
-
-Their presence is cherished, and their wisdom is sought after to ensure the continuity and preservation of Indigenous traditions. Native American women and elders are the bearers of cultural heritage, embodying the strength, resilience, and wisdom that enriches their communities and inspires future generations.
-"
-      bg="#fff"
-      hcol="#32773f"
-      tcol="#000"
-      align="left"
-      imgUrl="education/nawomen.png"
-    />
-
-    <EduElement
-      id="warming"
-      title="Global Warming"
-      desc="Global warming presents a multitude of complex challenges that have far-reaching impacts on Native American communities across the globe. These communities, deeply rooted in their ancestral lands, face unique vulnerabilities and are disproportionately affected by the consequences of climate change. Rising temperatures, changing precipitation patterns, and increased frequency of extreme weather events are just some of the consequences that Native American communities experience firsthand.
-
-In addition to the direct impacts on livelihoods, global warming also affects the delicate balance between nature and spirituality within Native American communities. These communities have deep spiritual connections to the land, water bodies, and natural resources. Changes in ecosystems, such as the loss of sacred sites or the diminishing availability of medicinal plants, have profound implications for their cultural and spiritual practices. The loss of biodiversity and the degradation of ecosystems undermine the traditional knowledge passed down through generations and jeopardize the integrity of their cultural heritage.
-
-Furthermore, the impacts of global warming exacerbate pre-existing socio-economic disparities faced by Native American communities. Many of these communities are located in rural and remote areas with limited access to resources and infrastructure.
-"
-      bg="#32773f"
-      hcol="#0f2413"
-      tcol="#fff"
-      align="right"
-      imgUrl="education/globalwarming.png"
-    />
+    <aside>
+      <EducationThumbnail />
+      <EducationThumbnail />
+    </aside>
+    <article>
+      <EducationMain />
+    </article>
   </main>
 </template>
 
@@ -103,6 +67,9 @@ Furthermore, the impacts of global warming exacerbate pre-existing socio-economi
 }
 
 .education {
+  background-color: #cbe1cf;
+  display: flex;
+
   transition: filter 0.5s;
   overflow: hidden;
 }
